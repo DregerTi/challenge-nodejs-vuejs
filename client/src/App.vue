c95e0cef0be40e97be1608ee4995ba59c3f77450
DregerTi <72619834+DregerTi@users.noreply.github.com>
2023-06-27 01:56:19 +0200

    button component & base css

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Button from "@/components/atoms/Button.vue";
+import Header from "@/components/organisms/Header.vue";
 </script>
 
 <template>
   <header>
+    <Header/>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -13,6 +16,16 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+
+        <Button title="dsdsd" icon="Settings"/>
+        <Button title="dsdsd" icon="Settings" variant="xl"/>
+        <Button title="dsdsd" icon="Settings" variant="bg-unset"/>
+        <Button title="dsdsd" icon="Settings" variant="xl--bg-unset"/>
+
+        <Button title="dsdsd" icon="Settings" variant="primary"/>
+        <Button title="dsdsd" icon="Settings" variant="xl--primary"/>
+        <Button title="dsdsd" icon="Settings" variant="xl--primary--bg-unset"/>
+
       </nav>
     </div>
   </header>
@@ -20,7 +33,14 @@ import HelloWorld from './components/HelloWorld.vue'
   <RouterView />
 </template>
 
-<style scoped>
+<style lang="scss">
+@function rem($pxVal){
+$base: 16;
+  @return calc($pxVal / $base) * 1rem;
+}
+
+
+
 header {
   line-height: 1.5;
   max-height: 100vh;
--- client/src/components/atoms/Button.vue
+++ client/src/components/atoms/Button.vue
@@ -1,14 +1,52 @@
 <script setup>
-  const { title, variant } = defineProps({
+  import IconAccountTree from "@/components/icons/IconAccountTree.vue";
+  import IconArrowDropDown from "@/components/icons/IconArrowDropDown.vue";
+  import IconArrowLeft from "@/components/icons/IconArrowLeft.vue";
+  import IconDashboard from "@/components/icons/IconDashboard.vue";
+  import IconArrowRight from "@/components/icons/IconArrowRight.vue";
+  import IconPerson from "@/components/icons/IconPerson.vue";
+  import IconSettings from "@/components/icons/IconSettings.vue";
+  import IconTimeline from "@/components/icons/IconTimeline.vue";
+  import IconTrackChanges from "@/components/icons/IconTrackChanges.vue";
+  import IconTrendingDown from "@/components/icons/IconTrendingDown.vue";
+  import IconTrendingUp from "@/components/icons/IconTrendingUp.vue";
+  import IconEdit from "@/components/icons/IconEdit.vue";
+
+  const { title, variant, icon } = defineProps({
     title: {
       type: String,
-      required: true
     },
     variant: {
       type: String,
       default: 'default',
       validator: (value) => {
-        return ['default', 'round', 'square'].includes(value);
+        return [
+          'default',
+          'round',
+          'square'
+        ].includes(value);
+      }
+    },
+    icon: {
+      type: String,
+      default: 'ArrowLeft',
+      validator: (value) => {
+        return [
+          'ArrowLeft',
+          'ArrowRight',
+          'AccountTree',
+          'ArrowDropDown',
+          'ArrowLeft',
+          'ArrowRight',
+          'Dashboard',
+          'Person',
+          'Settings',
+          'Timeline',
+          'TrackChanges',
+          'TrendingDown',
+          'TrendingUp',
+          'Edit'
+        ].includes(value);
       }
     },
     onClick: {
@@ -17,19 +55,6 @@
     }
   });
 
-  const style = {};
-  let realTitle = title;
-  switch (variant) {
-    case 'square':
-      style.borderRadius = '0';
-      break;
-    case 'round':
-      style.borderRadius = '50%';
-      style.textTransform = 'uppercase';
-      realTitle = title[0];
-      break;
-  }
-
   function handleClick() {
     alert('You clicked me!');
   }
@@ -37,22 +62,165 @@
 </script>
 
 <template>
-  <button v-bind:style="style" class="btn" v-on:click="onClick">{{ realTitle }}</button>
+  <button class="button"
+          :class="[variant ? 'button--' + variant : '']"
+          v-on:click="onClick">
+
+    <IconArrowLeft v-if="icon == 'ArrowLeft'"/>
+    <IconArrowRight v-if="icon == 'ArrowRight'"/>
+    <IconAccountTree v-if="icon == 'AccountTree'"/>
+    <IconArrowDropDown v-if="icon == 'ArrowDropDown'"/>
+    <IconArrowLeft v-if="icon == 'ArrowLeft'"/>
+    <IconArrowRight v-if="icon == 'ArrowRight'"/>
+    <IconDashboard v-if="icon == 'Dashboard'"/>
+    <IconPerson v-if="icon == 'Person'"/>
+    <IconSettings v-if="icon == 'Settings'"/>
+    <IconTimeline v-if="icon == 'Timeline'"/>
+    <IconTrackChanges v-if="icon == 'TrackChanges'"/>
+    <IconTrendingDown v-if="icon == 'TrendingDown'"/>
+    <IconTrendingUp v-if="icon == 'TrendingUp'"/>
+    <IconEdit v-if="icon == 'Edit'"/>
+
+    <p v-if="title">{{ title }}</p>
+  </button>
 </template>
 
-<style scoped>
-.btn {
-  display: inline-block;
-  padding: 0.5rem 1rem;
-  border-radius: 20px;
-  background-color: #000;
-  color: #fff;
-  text-decoration: none;
-  font-size: 1rem;
-  font-weight: 500;
-  line-height: 1.5;
-  text-align: center;
+<style scoped lang="scss">
+.button {
+  display: flex;
+  padding: 0.75rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.375rem;
   cursor: pointer;
-  transition: background-color 0.2s ease-in-out;
+  border:none;
+  background-color: var(--color-light-black);
+  color: var(--color-white);
+  font-weight: 700;
+  font-size: rem(16);
+  transition: all 0.2s ease-in-out;
+  border-radius: 10px;
+  font-size: 1rem;
+  line-height: 1rem;
+  text-transform: capitalize;
+
+  & svg{
+    min-width: 1.1em;
+    min-height: 1.1em;
+    width: 1.1em;
+    height: 1.1em;
+  }
+
+  &:hover {
+    background-color: var(--color-light-grey);
+    color: var(--color-light-black);
+  }
+
+  &--bg-unset{
+    background-color: unset;
+    color: var(--color-white);
+
+    &:hover {
+      background-color: unset;
+      color: var(--color-primary);
+    }
+  }
+
+  &--light-grey{
+    background-color: var(--color-light-grey);
+    color: var(--color-light-black);
+
+    &:hover {
+      background-color: var(--color-light-black);
+      color: var(--color-light-grey);
+    }
+
+    &--bg-unset{
+      background-color: unset;
+      color: var(--color-light-black);
+
+      &:hover {
+        background-color: unset;
+        color: var(--color-primary);
+      }
+    }
+  }
+
+  &--primary{
+    background-color: var(--color-primary);
+    color: var(--color-light-black);
+
+    &:hover {
+      background-color: var(--color-light-black);
+      color: var(--color-primary);
+    }
+
+    &--bg-unset{
+      background-color: unset;
+      color: var(--color-light-black);
+
+      &:hover {
+        background-color: unset;
+        color: var(--color-primary);
+      }
+    }
+  }
+
+  &--xl{
+    gap: 0.62rem;
+    font-size: 1.25rem;
+    line-height: 1.25rem;
+    border-radius: 14px;
+
+    & svg{
+      min-width: 1.25em;
+      min-height: 1.25em;
+      width: 1.25em;
+      height: 1.25em;
+    }
+
+    &--light-grey{
+      background-color: var(--color-light-grey);
+      color: var(--color-light-black);
+
+      &:hover {
+        background-color: var(--color-light-black);
+        color: var(--color-light-grey);
+      }
+
+      &--bg-unset{
+        background-color: unset;
+        color: var(--color-light-black);
+
+        &:hover {
+          background-color: unset;
+          color: var(--color-primary);
+        }
+      }
+    }
+
+    &--primary{
+      background-color: var(--color-primary);
+      color: var(--color-light-black);
+
+      &:hover {
+        background-color: var(--color-light-black);
+        color: var(--color-primary);
+      }
+
+      &--bg-unset{
+        background-color: unset;
+        color: var(--color-light-black);
+
+        &:hover {
+          background-color: unset;
+          color: var(--color-primary);
+        }
+      }
+    }
+
+  }
+
+
 }
 </style>
