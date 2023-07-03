29e2a089f6f5be36de2d0fb4f404424a1346d92c
DregerTi <72619834+DregerTi@users.noreply.github.com>
2023-07-04 01:43:48 +0200

    statRow & sttatRowData components

---
+++ client/src/components/atoms/StatRow.vue
@@ -0,0 +1,80 @@
+<script setup>
+import IconArrowUpward from "@/components/icons/IconArrowUpward.vue";
+import IconAdd from "@/components/icons/IconAdd.vue";
+import IconClose from "@/components/icons/IconClose.vue";
+import StatRowData from "@/components/atoms/StatRowData.vue";
+
+const { title, trend, value, ratio } = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  trend: {
+    type: String,
+    validator: (value) => {
+      return [
+        'up',
+        'down',
+        'same'
+      ].includes(value);
+    }
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+  ratio: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <li class="stat-row">
+    <header>
+      <h5>{{ title }}</h5>
+      <StatRowData :trend="trend" :value="value" :ratio="ratio" />
+    </header>
+    <div>
+      <div :style="'width:' + ratio + '%'"></div>
+    </div>
+  </li>
+</template>
+
+<style scoped lang="scss">
+.stat-row {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+
+  > header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    gap: 1.25rem;
+    > h5 {
+      color: var(--color-light-black);
+      font-size: 1.2rem;
+      font-weight: 500;
+      line-height: 1.2rem;
+    }
+  }
+
+  > div{
+    width: 100%;
+    height: 0.4rem;
+    border-radius: 0.25rem;
+    background-color: var(--color-dark-grey);
+    overflow: hidden;
+    > div{
+      height: 100%;
+      border-radius: 0.25rem;
+      background-color: var(--color-text);
+    }
+  }
+}
+</style>
---
+++ client/src/components/atoms/StatRowData.vue
@@ -0,0 +1,75 @@
+<script setup>
+import IconArrowUpward from "@/components/icons/IconArrowUpward.vue";
+import IconAdd from "@/components/icons/IconAdd.vue";
+import IconClose from "@/components/icons/IconClose.vue";
+
+const { trend, value, ratio } = defineProps({
+  trend: {
+    type: String,
+    validator: (value) => {
+      return [
+        'up',
+        'down',
+        'same'
+      ].includes(value);
+    }
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+  ratio: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="statRowData">
+    <span class="value">{{ value }}</span>
+    <IconArrowUpward class="trend trend--up" v-if="trend == 'up'"/>
+    <IconArrowUpward class="trend trend--down" v-if="trend == 'down'"/>
+    <IconClose class="trend trend--same" v-if="trend == 'same'"/>
+    <span class="ratio">{{ ratio }}%</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.statRowData{
+  display: flex;
+  gap: 0.5rem;
+  flex-wrap: nowrap;
+  align-items: center;
+
+  .value{
+    color: var(--color-text);
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1rem;
+  }
+
+  .ratio{
+    color: var(--color-text);
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1rem;
+  }
+
+  .trend{
+    width: 1rem;
+    &--up{
+      fill: var(--color-success);
+    }
+    &--down{
+      fill: var(--color-error);
+    }
+    &--same{
+      fill: var(--color-text);
+    }
+  }
+}
+
+</style>
