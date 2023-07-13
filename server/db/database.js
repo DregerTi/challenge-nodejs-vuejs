3ff5a8c8d86ea118ad641a119506d1cb5970df28
Gaël Stervinou <stervinou.g36@gmail.com>
2023-07-13 15:24:44 +0200

    fixed bdd + node migration on launch

--- server/db/database.js
+++ server/db/database.js
@@ -3,30 +3,38 @@ require('dotenv').config();
 NODE_ENV = process.env.NODE_ENV || "development";
 
 const config = {
-  development: {
-      databaseUrl: 'postgres://root:password@db:5432/app',
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: 'app',
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-  },
-  test: {
-      databaseUrl: 'postgres://root:password@127.0.0.1:5432/test',
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: 'app_test',
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-  },
-  production: {
-      databaseUrl: 'postgres://root:password@127.0.0.1:5432/prod',
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: 'app_production',
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-  },
+    development: {
+        databaseUrl: 'postgres://root:password@db:5432/app',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: 'app',
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+    },
+    testLocal: {
+        databaseUrl: 'postgres://root:password@db:5432/app',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: 'app_test',
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+    },
+    test: {
+        databaseUrl: 'postgres://root:password@127.0.0.1:5432/test',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: 'app_test',
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+    },
+    production: {
+        databaseUrl: 'postgres://root:password@127.0.0.1:5432/prod',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: 'app_production',
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+    },
 };
 
 module.exports = config[NODE_ENV];
