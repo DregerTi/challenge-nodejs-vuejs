fab1bb2371b9ad72e1a0cd0e5ecec4b30534f7a1
LouisAntoine <louisantoine.pro@gmail.com>
2023-07-28 12:20:26 +0200

    Mail update

--- server/utils/mailConfig.js
+++ server/utils/mailConfig.js
@@ -7,8 +7,8 @@ const transporter = nodemailer.createTransport({
     user: 'digitalanalyticsFR@gmail.com',
     clientId: process.env.MAIL_CLIENT_ID,
     clientSecret: process.env.MAIL_CLIENT_SECRET,
-    refreshToken: '1//04MKiFAXSEFRGCgYIARAAGAQSNwF-L9Ir-W1UxuI_ICilUgYfC7SOM_L4qZnHtmDmFDXBh_8vlUxccHWcGbptmJAQq5HG0Y2aiv4',
-    accessToken: 'ya29.a0AbVbY6MhYgd42FE5f_ik1Ea1S3F5dgmF2lNPaZqmbEzdaTjz0M99fkHBrcIphRwumiCmvPJBnpdTJ6xRQiloFsfG_lqo90ggzQq4Dmqc6e41DnnmxFLrrAg_1dJKV21olc2gilKfo5mrng_Q-RYOhtKjSUVjaCgYKAWMSARESFQFWKvPlsW2NeL0QJeqLKCHhnyhvLQ0163'
+    refreshToken: '1//04K1N24xU7gFtCgYIARAAGAQSNwF-L9IrXU_sTCpvxeg6_LFdraxtjPRP5haDau04WBRtQXWeL01mlA7o0fBs8WmO5Sq6eerwP6Q',
+    accessToken: 'ya29.a0AbVbY6PUNMf2QSQADmbXPSYeoUZBgY79XBEJfgUXswa2HxZoU5-5nCQzFM4IUwLV0J7T1SoBFM_otLqNdme5RNKr3d-_Bxm0VzovyivJfTG_V0-uu71s0AkuOaoYpS9jr0qP1g0RuLuGUS__LpWekpJrtI7OaCgYKAZASARESFQFWKvPltBn0mExaVVq21mfZGCChlg0163'
 }
 });
 
